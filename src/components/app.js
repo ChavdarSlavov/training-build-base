33b57806d6466adebaa511423a42431f714ae4be
Chavdar Slavov <chavdar.slavov@gmail.com>
2016-05-27 02:26:16 +0300

    Added linting

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './app.scss';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div 
-      	onClick={this.myClickHandler}
-      	className={styles.app}
-      >Hello world! {styles.mySpecialColor}</div>
-    );
+export default class App extends React.Component{
+
+  handleClick(){
+    console.log('Clicked');
   }
 
-  myClickHandler() {
-  	console.log('Clicked!');
+  render(){
+    return (
+      <div
+        onClick={this.handleClick}
+        className={styles.app}
+      >Hello world! {styles.mySpecialColor}</div>
+    );
   }
 }
