5865d9c96d613821493541c7903c225d9bae843a
Chavdar Slavov <chavdar.slavov@gmail.com>
2016-06-27 19:56:49 +0300

    Added editor configuration file for IDEs. Fixes for `npm start` command. Cleaned app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import styles from './app.scss';
 
 export default class App extends React.Component{
 
-  handleClick(){
-    console.log('Clicked');
-  }
-
   render(){
     return (
-      <div
-        onClick={this.handleClick}
-        className={styles.app}
-      >Hello world! {styles.mySpecialColor}</div>
+      <div>Hello world!</div>
     );
   }
 }
